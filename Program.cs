2d8597e6afbd3200440263e7fe9180cc08fdb0d9
muhkhansa <muhammadkhansa530@gmail.com>
2022-04-21 10:01:40 +0700

    responsi1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4514
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan = new Karyawan();
+
+            karyawan.Nik = 190302123;
+            karyawan.Nama = "Paijo";
+            karyawan.GajiBulanan = 3000000;
+
+            karyawan.NikB = 190302124;
+            karyawan.NamaB = "Jono";
+            karyawan.GajiBulananB = 2200000;
+
+            karyawan.KeteranganA();
+            karyawan.KeteranganB();
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
+
+
---
+++ ResponsiPemrograman4514.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4514
+{
+    public class Karyawan
+    {
+        public int Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+        public int NikB { get; set; }
+        public string NamaB { get; set; }
+        public int GajiBulananB { get; set; }
+        public void KeteranganA()
+        {
+            Console.WriteLine("No  Nik/Nama            Gaji Bulanan ");
+            Console.WriteLine("-------------------------------------");
+            Console.Write("1.{0} ", Nik, "{0} ", Nama, "{0}    ", GajiBulanan);
+            Console.WriteLine("2.{0} ", NikB, "{0} ", NamaB, "{0}    ", GajiBulananB);
+        }
+        public void KeteranganB()
+        {
+            Console.WriteLine("                                     ");
+            Console.WriteLine("Asyiiiiiik kenaikan gaji 10%!!");
+            Console.WriteLine("No  Nik/Nama            Gaji Bulanan ");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("1.{0} ", Nik, " {0}", Nama, "    {0}", GajiBulanan);
+            Console.WriteLine("2.{0} ", NikB, " {0}", NamaB, "    {0}", GajiBulananB);
+        }
+    }
+}
